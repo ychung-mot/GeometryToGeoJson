e84375b6b973bf08773c04f9cd631dc186dfe912
Young-Jin Chung <44654482+ychung-mot@users.noreply.github.com>
2020-03-10 11:13:41 -0700

    Initial commit

---
+++ GeometryToGeoJson/GeoDto.cs
@@ -0,0 +1,13 @@
+﻿using NetTopologySuite.Geometries;
+using NetTopologySuite.IO.Converters;
+using Newtonsoft.Json;
+
+namespace GeometryToGeoJson
+{
+    public class GeoDto
+    {
+        public string Name { get; set; }
+        [JsonConverter(typeof(GeometryConverter))]
+        public Geometry Geometry { get; set; }
+    }
+}
---
+++ GeometryToGeoJson/Program.cs
@@ -0,0 +1,33 @@
+﻿using NetTopologySuite;
+using NetTopologySuite.Geometries;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace GeometryToGeoJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+
+            var coordinates = new List<Coordinate>();
+
+            coordinates.Add(new Coordinate(-127.07202896, 54.82682777));
+            coordinates.Add(new Coordinate(-127.05364005, 54.82893557));
+
+            var dto = new GeoDto();
+            dto.Name = "First DTO";
+            dto.Geometry = geometryFactory.CreateLineString(coordinates.ToArray());
+
+            var json = JsonConvert.SerializeObject(dto, Formatting.Indented, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+
+            Console.WriteLine(json);
+            Console.ReadKey();
+        }
+    }
+}
